c79e7154252b03ed1b3d421d9a2b14a735d3b7a6
Shoan1309 <87231661+Shoan1309@users.noreply.github.com>
2026-03-28 22:39:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import tensorflow as tf
 import pickle
+import numpy as np
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense
 import re
-import h5py
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
@@ -13,33 +14,26 @@ def preprocess(text):
     review = re.sub('[^a-zA-Z]', ' ', text)
     review = review.lower()
     review = review.split()
-
     review = [ps.stem(word) for word in review if not word in stopwords.words('english')]
     review = ' '.join(review)
-
     return review
 
 ps = PorterStemmer()
 nltk.download('stopwords')
 
-import h5py
-with h5py.File("model.h5", "r") as f:
-    model = tf.keras.models.load_model(f, compile=False)
+model = tf.keras.models.load_model("model.h5", compile=False, safe_mode=False)
 
 with open("tokenizer.pkl", "rb") as f:
     tokenizer = pickle.load(f)
 
 st.title("Fake News Detection")
-
 user_input = st.text_area("Enter News Text")
 
 if st.button("Predict"):
     processed_text = preprocess(user_input)
     seq = tokenizer.texts_to_sequences([processed_text])
     pad = pad_sequences(seq, maxlen=20)
-
     prediction = model.predict(pad)[0][0]
-
     if prediction > 0.5:
         st.error("Fake News ❌")
     else:
